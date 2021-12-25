04622d24a5aa1ed3818b3ca326b1a01127e7f77c
cindy.choi <ssena310@gmail.com>
2021-12-26 00:08:36 +0900

    [SETTING] Add Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import RouterConfig from '@/routes/RouterConfig';
+import { GlobalStyle } from '@/globalStyle';
+import { ThemeContext } from '@/themes/context';
+import { useTheme } from '@/hooks/useTheme';
 
 function App() {
+  const { theme, toggleTheme, themeValues } = useTheme();
+
   return (
-    <RouterConfig />
+    <ThemeContext.Provider value={{ theme, toggleTheme, themeValues, }}>
+      <>
+        <GlobalStyle theme={themeValues} />
+        <RouterConfig />
+      </>
+    </ThemeContext.Provider>
   );
 }
 
